2b98bd7abe56d879dae41fd589ce3b503a8c1db8
Swapnil Yadav <46942236+yadavswap@users.noreply.github.com>
2023-04-11 20:22:43 +0530

    process config added

--- config/dbConect.js
+++ config/dbConect.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+
+import dotenv from "dotenv"
+dotenv.config()
 const dbConnect = async ()=>{
     try {
-        const connected =await mongoose.connect(process.env.MONGO_URL);
+        const connected =await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://swapnil:z7lvuDQNt0qlLfZz@cluster0.mgsmjoq.mongodb.net/nodejs-ecommerce-api");
         console.log(`Mongodb connected ${(connected).connection.host}`);
         
     } catch (error) {
